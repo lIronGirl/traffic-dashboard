39fa582a4b7f40d48dac9701d1fdc886cc9d7e6d
lIronGirl <1518583297@qq.com>
2020-02-11 22:38:34 +0800

    滚动表格完成

--- src/components/Rank.vue
+++ src/components/Rank.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
     <h3 class="align-right">出行量排行榜</h3>
-    <Table
-      id="rank-table"
-      stripe
-      :columns="rankColumns"
-      :data="tableData.concat(tableData).concat(tableData).concat(tableData)"
-    ></Table>
+    <Table id="rank-table" :height="viewHeight" stripe :columns="rankColumns" :data="tableData"></Table>
   </div>
 </template>
 
 <script>
+let tableDom = null;
+let timer = null;
 export default {
   data() {
     return {
@@ -33,7 +30,36 @@ export default {
           key: "address"
         }
       ],
-      tableData: [
+      tableData: [],
+      trHeight: 30,
+      trViewCount: 15
+    };
+  },
+  computed: {
+    viewHeight() {
+      return this.trHeight * this.trViewCount;
+    }
+  },
+  mounted() {
+    let that = this;
+    let table = document.getElementById("rank-table");
+    tableDom = table.getElementsByClassName("ivu-table-body")[0];
+    //   TODO调试接口时，需要修改为在回调中调用之后的函数
+    that.getTableData();
+    if (that.tableData.length > 15) {
+      that.startTimer();
+      tableDom.onmouseover = function() {
+        clearTimeout(timer);
+        timer = null;
+      };
+      tableDom.onmouseout = function() {
+        that.startTimer();
+      };
+    }
+  },
+  methods: {
+    getTableData() {
+      let fakeData = [
         {
           name: "John Brown",
           age: 18,
@@ -58,39 +84,37 @@ export default {
           address: "2231231",
           date: "2016-10-04"
         }
-      ]
-    };
-  },
-  mounted() {
-    setTimeout(function() {
-      var table = document
-        .getElementById("rank-table")
-        .getElementsByClassName("ivu-table-body")[0];
-      var timer = null;
-      function play() {
-        clearInterval(timer);
-        timer = setInterval(function() {
-          table.scrollTop++;
-          if (table.scrollTop >= 240 / 2) {
-            table.scrollTop = 0;
-          }
-        }, 100);
-      }
-      setTimeout(play, 500);
-      table.onmouseover = function() {
-        clearInterval(timer);
-      };
-      table.onmouseout = play;
-    }, 0);
+      ];
+
+      this.tableData = fakeData
+        .concat(fakeData)
+        .concat(fakeData)
+        .concat(fakeData)
+        .concat(fakeData);
+    },
+    startTimer() {
+      let that = this;
+      timer = setTimeout(() => {
+        tableDom.scrollTop++;
+        if (
+          tableDom.scrollTop + tableDom.offsetHeight >=
+          that.trHeight * that.tableData.length
+        ) {
+          var lastTimer = setTimeout(function() {
+            tableDom.scrollTop = 0;
+            clearTimeout(lastTimer);
+            clearTimeout(timer);
+            that.startTimer();
+          }, 1000);
+        } else {
+          clearTimeout(timer);
+          that.startTimer();
+        }
+      }, 50);
+    }
   }
 };
 </script>
-<style>
-#rank-table .ivu-table-body {
-  overflow: hidden;
-  height: 258px;
-}
-</style>
 <style lang="less" scoped>
 h3.align-right {
   text-align: right;
